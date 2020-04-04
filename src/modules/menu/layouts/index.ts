9e4cd931d0431ff998dab3afc14793ed83b44327
Naoki Ikeguchi <root@siketyan.dev>
2020-04-04 14:23:42 +0900

    ✨ Add menu layouts

---
+++ src/modules/menu/layouts/index.ts
@@ -0,0 +1,1 @@
+export { MenuLayoutComponent } from './menu-layout/menu-layout.component';
---
+++ src/modules/menu/layouts/menu-layout/menu-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-layout',
+  templateUrl: 'menu-layout.component.html',
+  styleUrls: [],
+})
+export class MenuLayoutComponent {
+}
